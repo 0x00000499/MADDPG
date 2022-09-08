908eeb5e51bcece5be3500992d0d9cc483589f21
Gao Xinzhi <651599228@qq.com>
2022-09-08 13:08:16 +0800

    version2.0 replay buffer
    将数组形式的buffer改为字典存储

--- test/replaybuffer_test.py
+++ test/replaybuffer_test.py
@@ -18,26 +18,16 @@ class TestReplayBuffer(unittest.TestCase):
         self.env.reset()
         self.env.render(mode='human')
 
-        self.n_agents = self.env.num_agents
-        self.agents = self.env.agents
-        self.state_dim = self.env.state_space.shape[0]
-        self.agent_obs_dims = []
-        self.agent_action_dims = []
         self.max_size = 1024
         self.batch_size = 2
-        for agent_idx in range(self.n_agents):
-            self.agent_obs_dims.append(self.env.observation_space(self.env.agents[agent_idx]).shape[0])
-            self.agent_action_dims.append(self.env.action_space(self.env.agents[agent_idx]).shape[0])
 
-        self.replay_buffer = ReplayBuffer(max_size=self.max_size, state_dim=self.state_dim,
-                                          agent_obs_dims=self.agent_obs_dims, agent_actions_dims=self.agent_action_dims,
-                                          n_agents=self.n_agents, batch_size=self.batch_size, agents=self.agents)
+        self.replay_buffer = ReplayBuffer(max_size=self.max_size, batch_size=self.batch_size, env=self.env)
 
     def test_sample_add(self):
         obs = self.env.reset()
         state = self.env.state()
         for step in range(self.max_cycles):
-            actions = {agent: [1, 0, 0, 0, 0] for agent in self.env.agents}
+            actions = {agent: [0, 0, 1, 0, 0] for agent in self.env.agents}
             obs_next, rewards, dones, _ = self.env.step(actions)
             state_next = self.env.state()
             self.replay_buffer.push(obs=obs, state=state, actions=actions, rewards=rewards, obs_next=obs_next,

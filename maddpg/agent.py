c94698bd7791babb9efc61658319030d11ed8750
Gao Xinzhi <651599228@qq.com>
2022-09-14 18:29:48 +0800

    agent complete

--- maddpg/agent.py
+++ maddpg/agent.py
@@ -5,32 +5,38 @@
 # @File : agent.py
 # @Project : maddpg-review
 import torch
-from networks import ActorNetwork, CriticNetwork
+from maddpg.networks import ActorNetwork, CriticNetwork
 
 
 class Agent:
-    def __init__(self, agent_obs_dims, state_dims, n_actions, n_agents, agent_name, alpha=0.01, beta=0.01, fc1=64, fc2=64,
+    def __init__(self, agent_obs_dims, state_dims, n_actions, n_agents, agent_name, save_dir, device, alpha=0.01,
+                 beta=0.01, fc1=64, fc2=64,
                  gamma=0.95, tau=0.01):
         self.gamma = gamma
         self.tau = tau
         self.n_actions = n_actions
         self.agent_name = agent_name
-        self.actor = ActorNetwork(alpha, agent_obs_dims, fc1, fc2, n_actions, save_dir="")
-        self.target_actor = ActorNetwork(alpha, agent_obs_dims, fc1, fc2, n_actions, save_dir="")
+        self.save_dir = save_dir
+        self.device = device
+        self.actor = ActorNetwork(alpha, agent_obs_dims, fc1, fc2, n_actions, device=self.device , save_dir=self.save_dir +agent_name +'_actor.pth')
+        self.target_actor = ActorNetwork(alpha, agent_obs_dims, fc1, fc2, n_actions, device=self.device ,save_dir=self.save_dir + agent_name + 'target_actor.pth')
 
-        self.critic = CriticNetwork(beta, state_dims, fc1, fc2, n_agents, n_actions, save_dir="")
-        self.target_critic = CriticNetwork(beta, state_dims, fc1, fc2, n_agents, n_actions, save_dir="")
+        self.critic = CriticNetwork(beta, state_dims, fc1, fc2, n_agents, n_actions, device=self.device ,save_dir=self.save_dir + agent_name + 'critic.pth')
+        self.target_critic = CriticNetwork(beta, state_dims, fc1, fc2, n_agents, n_actions, device=self.device ,save_dir=self.save_dir + agent_name  + 'target_critic.pth')
         # 根据参数可以调整为软更新或者直接更新,tau=1就是硬更新=0不更新
         self.update_network_parameters(tau=1)
 
-
     def choose_action(self, obs):
-        obs = torch.tensor(obs, dtype=torch.float)
-        action = self.actor.forward(obs)
+        obs = torch.tensor(obs, dtype=torch.float, device=self.device)
+        action = self.actor(obs)
         # TODO:添加噪声，而且这里还可能出问题
-        noise = 0
+        noise = torch.rand(self.n_actions, device=self.device)
         action = action + noise
-        return action.detach().numpy[0]
+        action = torch.clip(action, 0, 1)
+        if self.device == 'cuda':
+            return action.cpu().detach().numpy()
+        else:
+            return action.detach().numpy()
 
     def update_network_parameters(self, tau):
         for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
@@ -55,22 +61,3 @@ class Agent:
         self.target_actor.load_actor_model()
         self.critic.load_critic_model()
         self.target_critic.load_critic_model()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
